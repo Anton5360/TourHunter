5585fc66c77fce2eefe857b6a998fe0575c12318
Anton5360 <72033639+Anton5360@users.noreply.github.com>
2021-01-24 20:27:00 +0200

    Delete yii2/basic directory

--- yii2/basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => \yii\db\Connection::class,
-    'dsn' => 'mysql:host=TourHunter_db;dbname=TourHunter_db',
-    'username' => 'TourHunter_user',
-    'password' => 'TourHunter_pwd',
-    'charset' => 'utf8',
-];
